799f2148c24c4e4beff5780656682226dce6dd63
Amirul Azim <47412407+sovayle@users.noreply.github.com>
2021-05-01 15:55:08 +0800

    Create 2darray.cs

---
+++ 2darray.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2d_arrayz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numberGrid = {
+            
+                {1, 2 },
+                {3, 4 },
+                {5, 6 }
+            };
+
+            Console.WriteLine(numberGrid[1, 1]);
+
+            Console.ReadLine();
+            //HEY ITS ONLY A 3 by 2 ARRAY
+        }
+    }
+}
