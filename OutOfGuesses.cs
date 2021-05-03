32f0359bc3befa28e4d705e1ec3e01781aa10aba
Amirul Azim <47412407+sovayle@users.noreply.github.com>
2021-05-03 12:33:20 +0800

    Create OutOfGuesses.cs

---
+++ OutOfGuesses.cs
@@ -0,0 +1,29 @@
+# OutOfGuesses.cs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace outofguesssses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            for (int r = 1; r <=5; r++)
+            {
+                Console.WriteLine(r);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
