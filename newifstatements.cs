1c23be06c4d13f928bc338c9e04928397abf15c5
Amirul Azim <47412407+sovayle@users.noreply.github.com>
2021-05-03 12:32:12 +0800

    Update newifstatements.cs

--- newifstatements.cs
+++ newifstatements.cs
@@ -1,32 +1,39 @@
-using System; 
-using System.Collections.Generic; 
-using System.Linq; using System.Text; 
+# newifstatements.cs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
-namespace newifstates { class Program { static void Main(string[] args) {
+namespace newifstates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
-          bool isMale = false;
-          bool isTall = false;
+            bool isMale = false;
+            bool isTall = false;
 
-          if (isMale && isTall)
-          {
-              Console.WriteLine("You are tall and male");
-          }
-          else if (isMale && !isTall)
-          {
-              Console.WriteLine("You are a short male");
-          }
-          else if (!isMale && isTall)
-          {
-              Console.WriteLine("You are not male but you are tall");
-          }
-          else
-          {
-              Console.WriteLine("You are neither male or tall");
-          }
+            if (isMale && isTall)
+            {
+                Console.WriteLine("You are tall and male");
+            }
+            else if (isMale && !isTall)
+            {
+                Console.WriteLine("You are a short male");
+            }
+            else if (!isMale && isTall)
+            {
+                Console.WriteLine("You are not male but you are tall");
+            }
+            else
+            {
+                Console.WriteLine("You are neither male or tall");
+            }
 
-          Console.ReadLine();
+            Console.ReadLine();
 
-      }
-  }
+        }
+    }
 }
